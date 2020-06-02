1229bd692113a6f235d92fa054928a3608694d3c
tomeksen <tomy_trt@hotmail.com>
2020-06-02 10:42:50 +0200

    Subo av7

--- myfirstApp/src/app/home/home.page.ts
+++ myfirstApp/src/app/home/home.page.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+interface IProducto {
+  nombreProd: string,
+  descProd: string,
+  cateProd: string,
+  precioProd: number,
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +15,16 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  arrayProductos : Array<IProducto> = [];
+  producto: IProducto = {
+    nombreProd: "",
+    descProd: "",
+    cateProd: "",
+    precioProd: 0,
+  };
 
+  uploadproduct(product: IProducto){
+      this.arrayProductos.push(product)
+      alert(product.nombreProd+" se ha insertado correctamente")
+  }
 }
